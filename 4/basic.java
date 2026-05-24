01a1d07e5cc3076e025a249454f7588028165525
jaydeep-thakor <imjaydeepcreation@gmail.com>
2026-05-24 22:26:26 +0530

    first commit

---
+++ 4/basic.java
@@ -0,0 +1,13 @@
+public class basic {
+
+    // main function is a entry point
+    // void main(){ // can be written like this but public static void main is a classical way
+
+    public static void main(){
+        System.out.println("jaydeep");
+        System.out.println("hello" + " world");
+        System.out.println(3+4);
+    }
+
+}
+
