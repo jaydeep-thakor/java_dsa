ecae9f1209340f0bc1c8542a5ee678e404c5e670
Jaydeep <75525428+jaydeep-thakor@users.noreply.github.com>
2026-05-26 16:14:43 +0530

    Add Datatypes.java with examples of data types

---
+++ Datatypes.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+class Variables{
+    public static void main(String args[]){
+
+        // ********************** primitive datatypes **********************
+
+        int a = 10;
+        byte b = 10;
+        short c = 10;
+        long d = 10000L;
+
+        float e = 10.5F;
+        double f = 10.10;
+
+        char g = 'j';
+        boolean h = true;
+
+        // ********************** non primitive datatypes **********************
+
+        String s = "jaydeep";
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(s);
+        System.out.println(Arrays.toString(arr));
+        
+    }
+}
