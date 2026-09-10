8f4725552f23d7f1cb24e2d716fdb799ef01242e
jaydeep-thakor <imjaydeepcreation@gmail.com>
2026-09-10 23:09:10 +0530

    add lecture 9 code

---
+++ 9/Conditionals.java
@@ -0,0 +1,26 @@
+public class Conditionals{
+    public static void main(String[] args) {
+        
+        int score = 42;
+        
+        // if else
+        if(score >= 50){
+            System.out.println("pass");
+        }else{
+            System.out.println("false");
+        }
+
+        // if else if ladder
+        int accuracy = 78;
+        if(accuracy >= 90){
+            System.out.println("Excellent");
+        }else if(accuracy >= 75){
+            System.out.println("Good");
+        }else if(accuracy >= 60){
+            System.out.println("Average");
+        }else{
+            System.out.println("Needs Improvement");
+        }
+
+    }
+}
