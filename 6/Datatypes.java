c5edae7f75f8e88e72b90788064c24d86dd83c6a
Jaydeep <75525428+jaydeep-thakor@users.noreply.github.com>
2026-09-11 10:40:45 +0530

    Refactor Datatypes.java with examples and comments
    Updated primitive and non-primitive datatype examples, added type casting explanation.

--- 6/Datatypes.java
+++ 6/Datatypes.java
@@ -3,46 +3,43 @@ import java.util.Arrays;
 class Datatypes {
     public static void main(String[] args) {
 
-        // ********************** primitive datatypes **********************
-
-        int a = 10;
-        byte b = 10;
-        short c = 10;
-        long d = 10000L;
-
-        float e = 10.5F; // 'F' tells Java that 10.5 is a float literal; float has about 6–7 decimal digits of precision.
-        double f = 10.10; // Decimal values are double by default, so no suffix is required. double provides about 15–16 significant decimal digits of precision.
-
-        char g = 'j';
-        boolean h = true;
-
-        // ********************** non primitive datatypes **********************
-
-        String s = "jaydeep";
-        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        // Primitive datatypes
+        
+        // numeric types
+        byte a = 10;
+        short b = 20;
+        int c = 30;
+        long d = 10000000000L;
 
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
         System.out.println(d);
+
+        float e = 10.10F; // 'F' tells Java that 10.5 is a float literal; float has about 6–7 decimal digits of precision.
+        double f = 20.20; // Decimal values are double by default, so no suffix is required. double provides about 15–16 significant decimal digits of precision.
+
         System.out.println(e);
         System.out.println(f);
+
+        // non numeric types
+        Boolean g = true;
+        char h = 'j';
+
         System.out.println(g);
         System.out.println(h);
-        System.out.println(s);
-        System.out.println(Arrays.toString(arr));
+        
+        // Non-Primitive datatypes
+        int[] arr = {1,2,3,4,5,6,7,8};
+        String str = "Jaydeep";
+
+        System.out.println(arr);
+        System.out.println(Arrays.toString(arr)); // we have to use Arrays.toString to read the elements
+        System.out.println(str);
 
         // Type conversion
         char ch = 'a';
         System.out.println("Character after incrementing and type casting: " + (char)(ch + 1));
-
-        // Implicit Conversion - Implicit conversion is when Java automatically converts a smaller data type into a larger data type without us writing any extra code.
-        // int a = 10;
-        // double b = a;
-
-        // Explicit Conversion - Explicit conversion is when we manually convert one data type into another using type casting.
-        // double a = 10.5;
-        // int b = (int) a;
         
     }
 }
