604b0e3b0a2ca9dd1f617e9ce9add5f5df655344
jaydeep-thakor <imjaydeepcreation@gmail.com>
2026-09-12 13:58:20 +0530

    add lecture 9 complete code, if else, else if, switch case

--- 9/Conditionals.java
+++ 9/Conditionals.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Conditionals{
     public static void main(String[] args) {
         
@@ -20,6 +22,73 @@ public class Conditionals{
         } else {
             System.out.println("Needs Improvement");
         }
+
+        int age = 22;
+        char gender = 'M';
+
+        if(gender == 'M'){
+            if(age >= 18){
+                System.out.println("You are male and age >= 18");
+            }else{
+                System.out.println("You are male and age <= 18");
+            }
+        }else{
+            if(age >= 18){
+                System.out.println("You are female and age >= 18");
+            }else{
+                System.out.println("You are female and age <= 18");
+            }
+        }
+
+        boolean hasSubscription = true;
+        int solvedProblems = 780;
+
+        if(hasSubscription){
+            if(solvedProblems >= 500){
+                System.out.println("Unblock Advanced Sheet");
+            }else{
+                System.out.println("Practice More Problems");
+            }
+        }else{
+            System.out.println("Upgrade to Premium");
+        }
+
+        // Ternary operator
+        int streakDays = 200;
+        String status = (streakDays >= 100) ? "Consistent" : "Irregular";
+        System.out.println(status);
+
+        // Switch case statement
+        System.out.println("Enter the value of day = ");
+        Scanner sc = new Scanner(System.in);
+        int day = sc.nextInt();
+
+        switch(day){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default: 
+                System.out.println("Invalid input");
+        }
+        
 
     }
 }
