41f290c92d2a45daaeff87178e3482159627cd28
jaydeep-thakor <imjaydeepcreation@gmail.com>
2026-09-14 23:05:35 +0530

    add lecture 10 code

---
+++ 10/Loop.java
@@ -0,0 +1,15 @@
+public class Loop{
+
+    public static void main(String[] args) {
+        
+        for(int i = 1; i<=5; i++){
+            System.out.println("Value of i : " + i);
+        }
+
+        for(int i = 1; i<=10; i+=2){
+            System.out.println(i);
+        }
+
+    }
+
+}
