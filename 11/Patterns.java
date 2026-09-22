9d310161447e62260a5c5f93d7ebde25ec6c4489
jaydeep-thakor <imjaydeepcreation@gmail.com>
2026-09-22 22:29:18 +0530

    add lecture 11 code a pattern

--- 11/Patterns.java
+++ 11/Patterns.java
@@ -84,7 +84,25 @@ public class Patterns{
             }
             System.out.println(" ");
         }
+System.out.println();
+        int n8 = 10;
+        for(int row = 1; row<=n8; row++){
+            if(row == 1 || row == 2 || row == 10){
+                for(int col = 1; col<=row; col++){
+                    System.out.print("* ");
+                }
+            }else{
+                System.out.print("* ");
 
+                for(int col = 1; col<=(row-2); col++){
+                    System.out.print("  ");
+                }
+
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
     }
 
 }
